110cca66118d18d7419b11695169099c0b7c5f2a
Mikhail Talapila <mikhailtalapila@yahoo.com>
2025-07-19 22:54:59 -0400

    started notifications ux

--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -4,18 +4,17 @@ import AuctionCard from "./AuctionCard";
 import AppPagination from "../components/AppPagination";
 import { getData } from "../actions/auctionActions";
 import { useEffect, useState } from "react";
-import { Auction, PagedResult } from "@/types";
 import Filters from "./Filters";
 import { useParamsStore } from "@/hooks/useParamsStore";
 import { useShallow } from "zustand/shallow";
 import qs from "query-string";
 import EmptyFilter from "../components/EmptyFilter";
+import { useAuctionStore } from "@/hooks/useAuctionStore";
 
 
 
 export default function Listings() {
-
-  const [data, setData] = useState<PagedResult<Auction>>();
+  const [loading, setLoading] = useState(true); 
   const params = useParamsStore(useShallow(state => ({
     pageNumber: state.pageNumber,
     pageSize: state.pageSize,
@@ -26,6 +25,12 @@ export default function Listings() {
     winner: state.winner
   })));
 
+  const data  = useAuctionStore(useShallow(state => ({
+    auctions: state.auctions,
+    totalCount: state.totalCount,
+    pageCount: state.pageCount
+  })))
+  const setData  = useAuctionStore(state => state.setData);
   const setParams = useParamsStore(state => state.setParams);
   const url = qs.stringifyUrl({ url: '', query: params }, { skipEmptyString: true });
 
@@ -36,10 +41,11 @@ export default function Listings() {
   useEffect(() => {
     getData(url).then(data => {
       setData(data);
+      setLoading(false);
     })
-  }, [url]);
+  }, [url, setData]);
 
-  if (!data) return <h3>Loading...</h3>
+  if (loading) return <h3>Loading...</h3>
 
   return (
     <>
@@ -49,7 +55,7 @@ export default function Listings() {
       ) : (
         <>
           <div className="grid grid-cols-4 gap-6">
-            {data && data.results.map((auction) => (
+            {data && data.auctions.map((auction) => (
               <AuctionCard key={auction.id} auction={auction} />
             ))}
           </div>
